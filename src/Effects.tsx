1c0ee451e0b236610147b4b34732948aef1ef1fb
sg.sharov <sg.sharov@tensor.ru>
2022-03-20 23:47:08 +0300

    задание render and effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
+
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [currentValue, setCurrentValue] = useState(-1);
+
+    const changeMessage = (message: number): void => {
+        console.log(message);
+        setCurrentValue(message);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, changeMessage);
+        return () => {
+            unsubscribe(props.sourceId, changeMessage);
+            setCurrentValue(-1);
+        };
+    }, [props.sourceId]);
+    return <div>{`${props.sourceId}: ${currentValue}`}</div>;
 }
